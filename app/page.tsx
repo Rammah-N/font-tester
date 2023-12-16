89f2228fb9caab571d55429e8ab59654f3abf73d
Rammah <ramahnore@gmail.com>
2023-12-16 18:00:46 +0400

    refactor state reducer

--- app/page.tsx
+++ app/page.tsx
@@ -20,83 +20,50 @@ const reducer = (
 	state: CardProps,
 	action: { type: actionType; payload?: any }
 ) => {
+	const key = action.type.replace("+", "").replace("-", "");
+	const value = state[key as keyof CardProps];
 	switch (action.type) {
 		case "bg":
-			return { ...state, bg: action.payload };
+		case "hColor":
+		case "pColor":
+			return { ...state, [key]: action.payload };
 
 		case "hSize+":
-			return { ...state, hSize: state.hSize + 1 };
+		case "pSize+":
+		case "hLS+":
+		case "pLS+":
+			return { ...state, [key]: (value as number) + 1 };
+
 		case "hSize-":
-			if (state.hSize - 1 > 0) {
-				return { ...state, hSize: state.hSize - 1 };
+		case "pSize-":
+		case "hLS-":
+		case "pLS-":
+			if ((value as number) - 1 > 0) {
+				return { ...state, [key]: (value as number) - 1 };
 			}
 			return state;
 
 		case "hWeight+":
-			if (state.hWeight + 100 <= 900) {
-				return { ...state, hWeight: state.hWeight + 100 };
+		case "pWeight+":
+			if ((value as number) + 100 <= 900) {
+				return { ...state, [key]: (value as number) + 100 };
 			}
 			return state;
 		case "hWeight-":
-			if (state.hWeight - 100 >= 100) {
-				return { ...state, hWeight: state.hWeight - 100 };
+		case "pWeight-":
+			if ((value as number) - 100 >= 100) {
+				return { ...state, [key]: (value as number) - 100 };
 			}
 			return state;
-
-		case "hColor":
-			return { ...state, hColor: action.payload };
 
 		case "hLH+":
-			return { ...state, hLH: state.hLH + 0.1 };
-		case "hLH-":
-			if (state.hLH - 0.1 > 0) {
-				return { ...state, hLH: state.hLH - 0.1 };
-			}
-			return state;
-
-		case "hLS+":
-			return { ...state, hLS: state.hLS + 1 };
-		case "hLS-":
-			if (state.hLS - 1 > -1) {
-				return { ...state, hLS: state.hLS - 1 };
-			}
-			return state;
-
-		case "pSize+":
-			return { ...state, pSize: state.pSize + 1 };
-		case "pSize-":
-			if (state.pSize - 1 > 0) {
-				return { ...state, pSize: state.pSize - 1 };
-			}
-			return state;
-
-		case "pWeight+":
-			if (state.pWeight + 100 <= 900) {
-				return { ...state, pWeight: state.pWeight + 100 };
-			}
-			return state;
-		case "pWeight-":
-			if (state.pWeight - 100 >= 100) {
-				return { ...state, pWeight: state.pWeight - 100 };
-			}
-			return state;
-
-		case "pColor":
-			return { ...state, pColor: action.payload };
-
 		case "pLH+":
-			return { ...state, pLH: state.pLH + 0.1 };
-		case "pLH-":
-			if (state.pLH - 0.1 > 0) {
-				return { ...state, pLH: state.pLH - 0.1 };
-			}
-			return state;
+			return { ...state, [key]: (value as number) + 0.1 };
 
-		case "pLS+":
-			return { ...state, pLS: state.pLS + 1 };
-		case "pLS-":
-			if (state.pLS - 1 > -1) {
-				return { ...state, pLS: state.pLS - 1 };
+		case "hLH-":
+		case "pLH-":
+			if ((value as number) - 0.1 > 0) {
+				return { ...state, [key]: (value as number) - 0.1 };
 			}
 			return state;
 
@@ -107,7 +74,7 @@ const reducer = (
 
 const initialState = {
 	hSize: 32,
-	bg: "transparent",
+	bg: "#ffffff",
 	hColor: "#000",
 	hWeight: 700,
 	hLH: 1,
@@ -249,152 +216,168 @@ export default function Home() {
 	};
 
 	return (
-		<main className="flex flex-col">
-			<LayoutGroup>
-				{shown && (
-					<motion.div
-						initial={{ y: 0 }}
-						animate={{ y: [0, -5, 0] }} // Bouncing animation
-						transition={{ repeat: 10, duration: 0.5 }}
-						className="flex justify-center absolute top-10 right-10 clear">
-						<Button
-							variant="link"
-							onClick={() => setClearMode((prev) => !prev)}>
-							{clearMode ? "show" : "hide"} stuff
-						</Button>
-					</motion.div>
-				)}
-				<AnimatePresence>
-					{!clearMode && (
+		<>
+			<motion.main
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				className="text-center md:hidden">
+				<h1 className="text-7xl font-bold px-4">
+					The simplest font testing app
+				</h1>
+				<p className="mt-5 text-lg">
+					We all know there’s too many fonts out there,
+					<br /> so here’s an app that might make your life a bit easier <br />
+					that unfortunately only works on desktops <i>(for now)</i>
+				</p>
+			</motion.main>
+			<main className="flex-col hidden md:flex">
+				<LayoutGroup>
+					{shown && (
 						<motion.div
-							initial={{ opacity: 1 }}
-							animate={{ opacity: 1 }}
-							exit={{ opacity: 0 }}
-							className={cn("text-center", inter.className)}>
-							<motion.div initial={{ opacity: 1 }} exit={{ opacity: 0 }}>
-								<h1 className="text-7xl font-bold px-4">
-									The simplest font testing app
-								</h1>
-								<p className="mt-3 text-lg">
-									We all know there’s too many fonts out there,
-									<br /> so here’s an app that might make your life a bit easier
-								</p>
-							</motion.div>
-							<div className="flex justify-center mt-5 h-[40px] relative">
-								{shown && (
-									<motion.div
-										initial={{ width: 120, maxWidth: 120, opacity: 0 }}
-										animate={{
-											opacity: 1,
-											maxWidth: 300,
-											width: 300,
-										}}
-										exit={{ width: 120, maxWidth: 120, opacity: 0 }}
-										className="absolute top-0">
-										<Input
-											type="text"
-											name="search"
-											placeholder="Search Google fonts"
-											className="search z-20 w-full text-center border-black bg-transparent"
-											value={query}
-											onChange={onSearch}
-											autoComplete="off"
-										/>
-										{hoveredFont && (
-											<motion.div
-												initial={{ opacity: 0 }}
-												animate={{ opacity: 1 }}
-												exit={{ opacity: 0 }}
-												className="absolute top-0 w-fit -right-full p-4 rounded-md shadow-md z-[100] bg-[#faf9f6]">
-												<p
-													style={{
-														fontFamily: hoveredFont?.family || "Inter",
-														whiteSpace: "nowrap",
-													}}>
-													The quick brown fox jumps over the lazy dog
-												</p>
-											</motion.div>
-										)}
-										{searchResults.length > 0 && query && (
-											<motion.ul
-												initial={{ opacity: 0 }}
-												animate={{ opacity: 1 }}
-												exit={{ opacity: 0 }}
-												className="w-full shadow-md rounded-md bg-[#faf9f6] p-3 z-50 border-black border-[1px] mt-2 relative">
-												<List
-													className="w-full max-h-[200px] min-h-[100px]"
-													height={searchResults.length * 10}
-													width="auto"
-													itemData={searchResults}
-													itemCount={searchResults.length}
-													itemSize={40}>
-													{Row}
-												</List>
-											</motion.ul>
-										)}
-									</motion.div>
-								)}
-								{!shown && (
-									<motion.div
-										initial={{ opacity: 1, maxWidth: "auto" }}
-										exit={{ opacity: 0, maxWidth: 0 }}>
-										<Button className="start z-10" onClick={() => show(true)}>
-											Start Testing
-										</Button>
-									</motion.div>
-								)}
-							</div>
-							{shown && (
-								<motion.div
-									initial={{ opacity: 0, left: -50 }}
-									animate={{
-										opacity: 1,
-										left: 5,
-									}}
-									className="control-center fixed top-1/2 left-2 -translate-y-1/2 border-[2px] border-black p-5 bg-white rounded-xl shadow-lg scale-75">
-									<ControlCenter dispatch={dispatch} state={state} />
-								</motion.div>
-							)}
-							{shown && (
-								<motion.div
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									exit={{ opacity: 0 }}
-									className="flex items-center flex-col my-5 -z-1">
-									<span>Background Color</span>
-									<Input
-										type="color"
-										className="w-[100px]"
-										onChange={(e) =>
-											dispatch({ type: "bg", payload: e.target.value })
-										}
-										value={state.bg}
-									/>
-								</motion.div>
-							)}
+							initial={{ y: 0 }}
+							animate={{ y: [0, -5, 0] }} // Bouncing animation
+							transition={{ repeat: 10, duration: 0.5 }}
+							className="flex justify-center absolute top-10 right-10 clear">
+							<Button
+								variant="link"
+								onClick={() => setClearMode((prev) => !prev)}>
+								{clearMode ? "show" : "hide"} stuff
+							</Button>
 						</motion.div>
 					)}
-				</AnimatePresence>
-
-				<motion.ul
-					layout="position"
-					className="list flex flex-wrap w-full gap-5 justify-center mt-10 px-10">
 					<AnimatePresence>
-						{selectedFamilies.map((font, i) =>
-							shown ? (
-								<motion.div
-									className="inline max-w-fit"
-									style={{ flex: "1 1 25%" }}
-									key={font.family}
-									initial={{ opacity: 0, y: 100 }}
-									animate={{ opacity: 1, y: 0 }}>
-									<Card {...state} font={font} />
+						{!clearMode && (
+							<motion.div
+								initial={{ opacity: 1 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								className={cn("text-center", inter.className)}>
+								<motion.div initial={{ opacity: 1 }} exit={{ opacity: 0 }}>
+									<h1 className="text-7xl font-bold px-4">
+										The simplest font testing app
+									</h1>
+									<p className="mt-3 text-lg">
+										We all know there’s too many fonts out there,
+										<br /> so here’s an app that might make your life a bit
+										easier
+									</p>
 								</motion.div>
-							) : null
+								<div className="flex justify-center mt-5 h-[40px] relative">
+									{shown && (
+										<motion.div
+											initial={{ width: 120, maxWidth: 120, opacity: 0 }}
+											animate={{
+												opacity: 1,
+												maxWidth: 300,
+												width: 300,
+											}}
+											exit={{ width: 120, maxWidth: 120, opacity: 0 }}
+											className="absolute top-0">
+											<Input
+												type="text"
+												name="search"
+												placeholder="Search Google fonts"
+												className="search z-20 w-full text-center border-black bg-transparent"
+												value={query}
+												onChange={onSearch}
+												autoComplete="off"
+											/>
+											{hoveredFont && (
+												<motion.div
+													initial={{ opacity: 0 }}
+													animate={{ opacity: 1 }}
+													exit={{ opacity: 0 }}
+													className="absolute top-0 w-fit -right-full p-4 rounded-md shadow-md z-[100] bg-[#faf9f6]">
+													<p
+														style={{
+															fontFamily: hoveredFont?.family || "Inter",
+															whiteSpace: "nowrap",
+														}}>
+														The quick brown fox jumps over the lazy dog
+													</p>
+												</motion.div>
+											)}
+											{searchResults.length > 0 && query && (
+												<motion.ul
+													initial={{ opacity: 0 }}
+													animate={{ opacity: 1 }}
+													exit={{ opacity: 0 }}
+													className="w-full shadow-md rounded-md bg-[#faf9f6] p-3 z-50 border-black border-[1px] mt-2 relative">
+													<List
+														className="w-full max-h-[200px] min-h-[100px]"
+														height={searchResults.length * 10}
+														width="auto"
+														itemData={searchResults}
+														itemCount={searchResults.length}
+														itemSize={40}>
+														{Row}
+													</List>
+												</motion.ul>
+											)}
+										</motion.div>
+									)}
+									{!shown && (
+										<motion.div
+											initial={{ opacity: 1, maxWidth: "auto" }}
+											exit={{ opacity: 0, maxWidth: 0 }}>
+											<Button className="start z-10" onClick={() => show(true)}>
+												Start Testing
+											</Button>
+										</motion.div>
+									)}
+								</div>
+								{shown && (
+									<motion.div
+										initial={{ opacity: 0, left: -50 }}
+										animate={{
+											opacity: 1,
+											left: 5,
+										}}
+										className="control-center fixed top-1/2 left-2 -translate-y-1/2 border-[2px] border-black p-5 bg-white rounded-xl shadow-lg scale-75">
+										<ControlCenter dispatch={dispatch} state={state} />
+									</motion.div>
+								)}
+								{shown && (
+									<motion.div
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										exit={{ opacity: 0 }}
+										className="flex items-center flex-col my-5 -z-1">
+										<span>Background Color</span>
+										<Input
+											type="color"
+											className="w-[100px]"
+											onChange={(e) =>
+												dispatch({ type: "bg", payload: e.target.value })
+											}
+											value={state.bg}
+										/>
+									</motion.div>
+								)}
+							</motion.div>
 						)}
 					</AnimatePresence>
-				</motion.ul>
-			</LayoutGroup>
-		</main>
+
+					<motion.ul
+						layout="position"
+						className="list flex flex-wrap w-full gap-5 justify-center mt-10 px-10">
+						<AnimatePresence>
+							{selectedFamilies.map((font, i) =>
+								shown ? (
+									<motion.div
+										className="inline max-w-fit"
+										style={{ flex: "1 1 25%" }}
+										key={font.family}
+										initial={{ opacity: 0, y: 100 }}
+										animate={{ opacity: 1, y: 0 }}>
+										<Card {...state} font={font} />
+									</motion.div>
+								) : null
+							)}
+						</AnimatePresence>
+					</motion.ul>
+				</LayoutGroup>
+			</main>
+		</>
 	);
 }
